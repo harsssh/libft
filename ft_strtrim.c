d5c923bbe12c46d48ac391910091559827499aa5
harsssh <66548698+harsssh@users.noreply.github.com>
2023-05-21 18:17:06 +0900

    fix strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,11 +6,12 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 09:43:59 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/05/20 19:22:12 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/05/21 18:16:57 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 // whether s contains c
 static int	contains(const char *s, char c)
@@ -20,18 +21,18 @@ static int	contains(const char *s, char c)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	start;
+	size_t	begin;
 	size_t	end;
 
 	if (s1 == NULL || set == NULL)
 		return (NULL);
-	start = 0;
-	while (s1[start] && contains(set, s1[start]))
-		start++;
-	end = ft_strlen(s1) - 1;
-	while (s1[end] && contains(set, s1[end]))
+	begin = 0;
+	while (s1[begin] && contains(set, s1[begin]))
+		begin++;
+	end = ft_strlen(s1);
+	while (begin < end && contains(set, s1[end - 1]))
 		end--;
-	if (end < start)
+	if (begin >= end)
 		return (ft_strdup(""));
-	return (ft_substr(s1, start, end - start + 1));
+	return (ft_substr(s1, begin, end - begin));
 }
